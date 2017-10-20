d937422b42522bea7bd9dfee8040be3157df3500
privat4k <tgk4kprivat@gmail.com>
2017-10-20 20:45:34 +0300

    trial commit

---
+++ alert.js
@@ -0,0 +1,21 @@
+/*
+коментарий
+ */
+"use strict";
+
+var first = 5;
+var second = 8;
+var result = first/second;
+alert(result);
+
+
+var message;
+
+// скопировали значение
+message = hello;
+
+
+var admin,name;
+name='Василий';
+admin=name;
+alert(admin);
