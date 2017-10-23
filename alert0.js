8d7aa0111cc53636d4ef9e82bfa6565e9d1edcd4
privat4k <tgk4kprivat@gmail.com>
2017-10-23 21:08:19 +0300

    hm

---
+++ alert0.js
@@ -0,0 +1,35 @@
+
+"use strict";
+
+
+var c = 15;
+var d = 2;
+var result = c+d;
+alert(result);
+
+var a = 10;
+var b = 2;
+var c1 = 5;
+alert(a+b+c1);
+
+var a1 = 17;
+var b1 = 10;
+var c2 = a1-b1;
+var d1 = 7;
+var result1 = c2+d1;
+alert(result1);
+
+var str = 'Привет, Мир!';
+alert(str);
+
+var str1 = 'Привет, ';
+var str2 = 'Мир!';
+alert(str1+str2);
+
+var name = 'Максим';
+alert('Привет, ' + name);
+
+var age = 24;
+alert('Мне' + age + 'года!');
+
+
