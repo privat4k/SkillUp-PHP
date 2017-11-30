28bd1b166f8fe07a3d6218348fa9617e2c9ed668
privat4k <tgk4kprivat@gmail.com>
2017-11-30 22:28:24 +0200

    mhj

--- rexexp.php
+++ rexexp.php
@@ -1,8 +1,13 @@
 <?php
 
-$str = 'dhpsdgpl lewt test@email.com lwetwv ewtkwel wet g dgdfhgjgfdkghk';
-$res = preg_match('/\w+@/',$str, $match);
+$str = 'dhpsdgpl <lewt> test@-email.com.ua lwetwv <ewtkwel> wet test@email.com g dgdfhgjgfdkghk';
+$res = preg_match_all('/[\w-\.]{1,}@[-a-zA-z0-9]+(\.[\w-]+)+/m', $str, $match);
 
 echo $res ? 'Найдено' : 'Не найдено';
 var_dump($match);
+
+$replaced = preg_replace('/(\.[\w-]+)+/', '.info', $str);
+echo $replaced . '<br>';
+$replaced = preg_replace('/<(.*)>/iuU', '($1)', $str);
+echo $replaced;
 
